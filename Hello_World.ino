574678d36e9f640a4ae7bb69ef7644a6756fcef6
helloworldlethavefun <morganpiper08@gmail.com>
2021-04-29 07:41:47 +1200

    Add files via upload

---
+++ Hello_World.ino
@@ -0,0 +1,20 @@
+//Example 01: Blinking led
+
+
+const int LED = 13; // LED connected to
+                    // digital pin 13
+
+
+void setup() {
+    pinMode(LED,OUTPUT); // sets the digital 
+                         // pin as output
+}
+
+void loop() {
+  
+  digitalWrite(LED, HIGH); // turns the LED on
+  delay(100);  // waits for a second
+  digitalWrite(LED, LOW); // turns the LED off
+  delay(100); // waits for a second
+    
+}
